54585302fed078df455b109de0b8d8556baad6e8
Julio Sandroni <sandronister@gmail.com>
2023-10-17 11:15:23 -0300

    create entitie bidirectional

--- internal/service/category.go
+++ internal/service/category.go
@@ -90,3 +90,24 @@ func (c *CategoryService) CreateCategoryStream(stream pb.CategoryService_CreateC
 			})
 	}
 }
+
+func (c *CategoryService) CreateCategoryStreamBidirectional(stream pb.CategoryService_CreateCategoryStreamBidirectionalServer) error {
+	for {
+		category, err := stream.Recv()
+
+		if err == io.EOF {
+			return nil
+		}
+
+		if err != nil {
+			return err
+		}
+
+		categoryResult, err := c.CategoryDB.Create(category.Name, category.Description)
+
+		if err != nil {
+			return err
+		}
+		stream.Send(&pb.Category{Id: categoryResult.ID, Name: categoryResult.Name, Description: categoryResult.Description})
+	}
+}
