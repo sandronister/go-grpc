11601c225660b9f8656b50fb2c9bed1f2c024a1c
Julio Sandroni <sandronister@gmail.com>
2023-10-17 10:50:27 -0300

    get category

--- internal/service/category.go
+++ internal/service/category.go
@@ -48,3 +48,17 @@ func (c *CategoryService) ListCategories(ctx context.Context, in *pb.Blank) (*pb
 
 	return &pb.CategoryList{Categories: listCategories}, nil
 }
+
+func (c *CategoryService) GetCategory(ctx context.Context, in *pb.CategoryRequest) (*pb.Category, error) {
+	category, err := c.CategoryDB.Find(in.Id)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &pb.Category{
+		Id:          category.ID,
+		Name:        category.Name,
+		Description: category.Description,
+	}, nil
+}
