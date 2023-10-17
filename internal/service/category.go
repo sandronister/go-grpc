0deeac266b34f79dd89963c4079510e20a1a53dd
Julio Sandroni <sandronister@gmail.com>
2023-10-17 10:12:56 -0300

    first commit

---
+++ internal/service/category.go
@@ -0,0 +1,30 @@
+package service
+
+import (
+	"context"
+
+	"github.com/sandronister/go-grpc/internal/database"
+	"github.com/sandronister/go-grpc/internal/pb"
+)
+
+type CategoryService struct {
+	pb.UnimplementedCategoryServiceServer
+	CategoryDB database.Category
+}
+
+func NewCategoryService(categoryDB database.Category) *CategoryService {
+	return &CategoryService{CategoryDB: categoryDB}
+}
+
+func (c *CategoryService) CreateCategory(ctx context.Context, in *pb.CreateCategoryRequest) (*pb.Category, error) {
+	category, err := c.CategoryDB.Create(in.Name, in.Description)
+	if err != nil {
+		return nil, err
+	}
+
+	return &pb.Category{
+		Id:          category.ID,
+		Name:        category.Name,
+		Description: category.Description,
+	}, nil
+}
