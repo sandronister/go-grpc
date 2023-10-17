f76c7ad12fa11cedd115a60a1d0c2824e5e2132e
Julio Sandroni <sandronister@gmail.com>
2023-10-17 11:04:36 -0300

    create entity stream unidirectional

--- internal/service/category.go
+++ internal/service/category.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"context"
+	"io"
 
 	"github.com/sandronister/go-grpc/internal/database"
 	"github.com/sandronister/go-grpc/internal/pb"
@@ -61,4 +62,31 @@ func (c *CategoryService) GetCategory(ctx context.Context, in *pb.CategoryReques
 		Name:        category.Name,
 		Description: category.Description,
 	}, nil
+}
+
+func (c *CategoryService) CreateCategoryStream(stream pb.CategoryService_CreateCategoryStreamServer) error {
+	categories := &pb.CategoryList{}
+
+	for {
+		category, err := stream.Recv()
+		if err == io.EOF {
+			return stream.SendAndClose(categories)
+		}
+
+		if err != nil {
+			return err
+		}
+
+		categoryResult, err := c.CategoryDB.Create(category.Name, category.Description)
+		if err != nil {
+			return nil
+		}
+
+		categories.Categories = append(categories.Categories,
+			&pb.Category{
+				Id:          categoryResult.ID,
+				Name:        categoryResult.Name,
+				Description: categoryResult.Description,
+			})
+	}
 }
